43cf9c3c50280a10eb5924b48f301adf466081a4
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-02-13 11:42:22 +0100

    add: add bootstrap form for add product

--- add.php
+++ add.php
@@ -1,4 +1,7 @@
 <?php
+
+// @TODO! - Créer Backoffice et intégrer la page ajout dans /admin/
+
 require_once __DIR__ . '/config/path.cfg.php';
 require_once __DIR__ . '/config/database.cfg.php';
 require_once __DIR__ . '/function/database.fn.php';
@@ -34,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Insère le nouveau produit dans la table 'products'
         $sql = "INSERT INTO `products` (`product_name`, `product_description`, `product_price`, `product_category_id`)
                 VALUES (:product_name, :product_description, :product_price, :product_category_id)";
-        
+
         // Préparation de la requête SQL pour l'exécution.
         // $db est l'objet de la base de données, et $sql est la chaîne de la requête SQL.
         // La méthode prepare() renvoie un objet 'statement' ($sth) qui peut être utilisé pour exécuter la requête.
@@ -92,31 +95,95 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "Erreur lors de l'ajout du produit : " . $e->getMessage();
     }
 }
+
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
 
-<form action="#" method="POST">
-    <label for="productName">Nom : </label> <input type="text" name="product_name" id="productName"><br>
-    <label for="productPrice">Prix : </label><input type="number" name="product_price" id="productPrice"><br>
-    <div class="form-floating">
-        <label for="productCategory" class="visually-hidden">Catégorie</label>
-        <select class="form-select" id="productCategory" name="product_category_id" aria-label="Sélection de la catégorie">
-            <option selected>Sélectionner une catégorie</option>
-            <?php foreach ($productCategories as $row) : ?>
-                <option value="<?= $row['id'] ?>"><?= $row['category_name'] ?></option>
-            <?php endforeach; ?>
-        </select>
-    </div>
-    <div class="form-floating">
-        <label for="doctor" class="visually-hidden">Médecins</label>
-        <select class="form-select" id="doctor" name="doctor_id" aria-label="Sélection du médecin">
-            <option selected>Sélectionner un médecin</option>
-            <?php foreach ($doctors as $row) : ?>
-                <option value="<?= $row['id'] ?>"><?= $row['doctor_name'] ?></option>
-            <?php endforeach; ?>
-        </select>
-    </div>
-    <label for="productPathImg">Fichier image : </label><input type="file" name="product_path_img" id="productPathImg"><br>
-    <label for="productDesc">Description : </label><textarea name="product_description" id="productDesc"></textarea><br>
-    <input type="submit" name="submitProduct" value="Ajouter">
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap Libraries -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <!-- CSS Custom -->
+    <link rel="stylesheet" href="<?= CSS_PATH ?>/styles.css">
+
+    <title>Back Office : Ajout de produits</title>
+</head>
+
+<body class="d-flex flex-column vh-100">
+
+    <main class="flex-grow-1 py-4">
+        <div class="container py-4">
+            <form action="#" method="POST" class="col-6 mx-auto">
+                <fieldset>
+                    <legend>Ajouter un produit</legend>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="productName" class="form-label">Nom : </label>
+                            <input type="text" name="product_name" id="productName" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="productPrice" class="form-label">Prix : </label>
+                            <input type="number" name="product_price" id="productPrice" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+
+                            <label for="productCategory" class="form-label visually-hidden">Catégorie</label>
+                            <select class="form-select" id="productCategory" name="product_category_id" aria-label="Sélection de la catégorie">
+                                <option selected>Sélectionner une catégorie</option>
+                                <?php foreach ($productCategories as $row) : ?>
+                                    <option value="<?= $row['id'] ?>"><?= $row['category_name'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="doctor" class="form-label visually-hidden">Médecins</label>
+                            <select class="form-select" id="doctor" name="doctor_id" aria-label="Sélection du médecin">
+                                <option selected>Sélectionner un médecin</option>
+                                <?php foreach ($doctors as $row) : ?>
+                                    <option value="<?= $row['id'] ?>"><?= $row['doctor_name'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="productPathImg" class="form-label">Fichier image : </label>
+                            <input type="file" name="product_path_img" id="productPathImg" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="productDesc" class="form-label">Description : </label>
+                            <textarea name="product_description" id="productDesc" class="form-control" rows="10"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input type="submit" name="submitProduct" value="Ajouter" class="btn btn-primary">
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+
+    </main>
+</body>
+</html>
